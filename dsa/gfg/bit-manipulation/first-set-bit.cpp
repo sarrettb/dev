f1bd43ba4cd744d812644c41f22c7a83e771ce52
Blake <blake.sarrett@gmail.com>
2022-12-06 17:27:50 -0600

    move to right location

--- dsa/gfg/bit-manipulation/first-set-bit.cpp
+++ dsa/gfg/bit-manipulation/first-set-bit.cpp
@@ -9,12 +9,14 @@ class Solution
     //Function to find position of first set bit in the given number.
     unsigned int getFirstSetBit(int n)
     {
-       return log2(n & ~(n - 1)) + 1;
+        // n & ~(n-1) returns 2^k where k is the first set bit with 0-indexing
+        // take log2() + 1 to find k with 1-indexing
+        return log2(n & ~(n - 1)) + 1;
     }
 };
 
 int main() {
-    vector<int> test_points = {12, 18};
+    vector<int> test_points = {18, 12};
     for (auto& t : test_points) {
         Solution sol;
         cout << sol.getFirstSetBit(t) << " ";
