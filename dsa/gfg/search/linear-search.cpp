7690f3deea5f519bce0cb99a5d95c9c53a4d91f2
Blake <blake.sarrett@gmail.com>
2022-12-08 07:07:17 -0600

    binary and linear

--- dsa/gfg/search/linear-search.cpp
+++ dsa/gfg/search/linear-search.cpp
@@ -4,7 +4,7 @@
 int search(int arr[], int N, int X) {
     for (int i = 0; i < N; i++)
         if (arr[i] == X)
-            return i + 1;
+            return i;
     return -1;
 }
 
@@ -12,7 +12,7 @@ int main() {
     std::vector<std::vector<int>> test_points = { {1, 2, 3, 4, 5},
                                                   {5, 4, 3, 2, 1} } ;
     for (auto& t : test_points) {
-        std::cout << "Position=" << search(t.data(), t.size(), 4) << std::endl;
+        std::cout << "Index=" << search(t.data(), t.size(), 4) << std::endl;
     }
     return EXIT_SUCCESS;
 }
