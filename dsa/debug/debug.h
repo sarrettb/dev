dc04ebd5381bfbfeb2d7e6118d789c73826d11f9
Blake <blake.sarrett@gmail.com>
2022-12-12 07:25:22 -0600

    debugging

---
+++ dsa/debug/debug.h
@@ -0,0 +1,27 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <fstream> 
+#include <vector> 
+#include <string>
+
+/* Debug file contains one line of text, space seperated */
+std::vector<int> read_debug(const char* debug_path) {
+   std::vector<int> debug_data;
+   std::ifstream debug_file;
+   std::string debug_string;
+   debug_file.open(debug_path);
+   if (debug_file.is_open()) {
+        while (debug_file.peek() != EOF) {
+            char c = debug_file.get();
+            if (c == ' ') {
+               debug_data.push_back(std::stoi(debug_string));
+               debug_string.clear();
+            }
+            else
+                debug_string.push_back(c);
+        }
+   }
+   return debug_data;
+}
+#endif 
