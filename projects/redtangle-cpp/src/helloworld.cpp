1144029334096cccd14280c1f43701258836615d
Blake <blake@blake.myguest.virtualbox.org>
2023-08-14 06:07:45 -0500

    removed

--- projects/redtangle-cpp/src/helloworld.cpp
+++ projects/redtangle-cpp/src/helloworld.cpp
@@ -3,9 +3,12 @@
 
 int main() {
     helloworld::HelloRequest request; 
+    helloworld::HelloResponse response;
     request.set_message("Hello Request!");
+    response.set_message("Hello Response!"); 
     std::cout << "Hello World!" << std::endl;
     std::cout << "helloworld.pb.h included!" << std::endl;
     std::cout << "HelloRequest Message: " << request.message() << std::endl;
+    std::cout << "HelloResponse Message: " << response.message() << std::endl;
     return 0;
 }
