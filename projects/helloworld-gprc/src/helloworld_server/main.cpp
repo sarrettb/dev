fdf577bafd2cf2b46dd07a611d6f624f4ebf74aa
bsarrett <blake.sarrett@gmail.com>
2023-08-20 21:13:18 -0500

    fix server

--- projects/helloworld-gprc/src/helloworld_server/main.cpp
+++ projects/helloworld-gprc/src/helloworld_server/main.cpp
@@ -4,12 +4,8 @@
 int main(int argc, char* argv[]) {
     HelloWorldServer server; 
     auto options = butility::parse_commandline(argc, argv); 
-    if (options.find("ip") != options.end()) {
-        server.set_address(options["ip"]); 
-    }
-    if (options.find("port") != options.end()) {
-        server.set_port(stoi(options["port"])); 
-    }
+    server.set_address("10.0.2.15");
+    server.set_port(50052); 
     server.start_server(); 
     std::cout << "Press any key to stop..." << std::endl;
     std::cin.get();
