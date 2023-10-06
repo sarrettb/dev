946cf26e2498e31677217cb9ab4405161072b74d
bsarrett <blake.sarrett@gmail.com>
2023-10-06 07:12:48 -0500

    rename docs

---
+++ projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_assert.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_assert.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_common.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_common.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_compare.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_compare.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_crc32.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_crc32.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_font.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_font.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_fuzzer.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_fuzzer.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_harness.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_harness.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_imageBlit.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageBlit.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_imageBlitBlend.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageBlitBlend.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_imageFace.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageFace.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_imagePrimitives.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_imagePrimitives.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_imagePrimitivesBlend.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_imagePrimitivesBlend.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_log.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_log.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_md5.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_md5.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_memory.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_memory.c.o"
+  "/home/bsarrett/dev/projects/redtangle-cpp/libs/SDL/src/test/SDL_test_random.c" "/home/bsarrett/dev/projects/redtangle-cpp/cmake/build/libs/SDL/CMakeFiles/SDL2_test.dir/src/test/SDL_test_random.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "libs/SDL/include"
+  "libs/SDL/include/SDL2"
+  "libs/SDL/include-config-/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
