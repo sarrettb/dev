064a23256310b1f4a139849b39932f737558c279
bsarrett <blake.sarrett@gmail.com>
2023-10-06 06:59:14 -0500

    git ignore

---
+++ projects/redtangle-cpp/cmake/build/src/CMakeFiles/redtangle.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/redtangle.dir/app/redtangle_app.cpp.o"
+  "CMakeFiles/redtangle.dir/game/redtangle_local.cpp.o"
+  "CMakeFiles/redtangle.dir/main.cpp.o"
+  "CMakeFiles/redtangle.dir/piece/empty_piece.cpp.o"
+  "CMakeFiles/redtangle.dir/piece/redtangle_piece.cpp.o"
+  "CMakeFiles/redtangle.dir/ui/redtangle_ui_imgui.cpp.o"
+  "CMakeFiles/redtangle.dir/ui/redtangle_ui_sdl.cpp.o"
+  "redtangle"
+  "redtangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/redtangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
